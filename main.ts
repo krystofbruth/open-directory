e356a5a2aeb706cd99d8f09c9f713e3ccf2c02e8
Kryštof Bruthans <bruthans.krystof11@gmail.com>
2024-12-15 11:56:28 +0100

    Refactor project structure, document basic auth endpoints in oapi_spec

--- main.ts
+++ main.ts
@@ -1,14 +1,1 @@
-import { Application, Router } from "@oak/oak";
 
-const app = new Application();
-const router = new Router();
-
-router.get("/", (ctx) => {
-  ctx.response.status = 201;
-  ctx.response.body = `Hello world!`;
-});
-
-app.use(router.routes());
-
-app.listen({ port: 4000 });
-console.log("App listening");
